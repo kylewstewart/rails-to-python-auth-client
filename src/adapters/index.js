d3413f3e48c10fa15d37bac084269706ac453765
kylewstewart <kyle.w.stewart@mac.com>
2017-12-22 12:44:03 -0500

    cors swithed to axios

--- src/adapters/index.js
+++ src/adapters/index.js
@@ -1,30 +1,56 @@
+import axios from 'axios';
+
 const baseURL = 'http://127.0.0.1:5000/api/v1';
 
-const headers = () => ({
-  'content-type': 'application/json',
-  accept: 'application/json',
-  Authorization: localStorage.getItem('token'),
-});
+const config = {
+  headers: {
+    'content-type': 'application/json',
+    accept: 'application/json',
+    Authorization: localStorage.getItem('token'),
+  },
+};
 
 export const auth = (username, password) => (
-  fetch(`${baseURL}/auth`, {
-    method: 'post',
-    headers: headers(),
-    body: JSON.stringify({ username, password }),
-  }).then(response => response.json())
-
+  axios.post(`${baseURL}/auth`, { username, password }, config)
+    .then(({ data }) => data)
 );
 
 export const currentUser = () => (
-  fetch(`${baseURL}/current_user`, {
-    headers: headers(),
-  }).then(response => response.json())
+  axios.get(`${baseURL}/auth`, config).then(({ data }) => data)
 );
 
+
 export const createUser = ({ username, password }) => (
-  fetch(`${baseURL}/user`, {
-    method: 'post',
-    headers: headers(),
-    body: JSON.stringify({ username, password }),
-  }).then(response => response.json())
+  axios.post(`${baseURL}/user`, { username, password }, config)
+    .then(({ data }) => data)
 );
+
+
+// const header = {
+//   'content-type': 'application/json',
+//   accept: 'application/json',
+//   Authorization: localStorage.getItem('token'),
+// };
+
+// export const auth = (username, password) => (
+//   fetch(`${baseURL}/auth`, {
+//     method: 'post',
+//     headers: headers(),
+//     body: JSON.stringify({ username, password }),
+//   }).then(response => response.json())
+//
+// );
+
+// export const currentUser = () => {
+//   return fetch(`${baseURL}/auth`, {
+//     headers: header,
+//   }).then(response => {debugger});
+// };
+
+// export const createUser = ({ username, password }) => (
+//   fetch(`${baseURL}/user`, {
+//     method: 'post',
+//     headers: headers(),
+//     body: JSON.stringify({ username, password }),
+//   }).then(response => response.json())
+// );
